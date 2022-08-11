3d2e686f3fa8c6c3f97e29269724b2a995b70941
jeongmu <hot8961@naver.com>
2022-08-11 15:41:19 +0900

    첨부파일 아이콘 버튼 출력

--- blog/models.py
+++ blog/models.py
@@ -15,4 +15,8 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_ext(self):
+        return self.get_file_ext().split('.')[-1]
+
 # Create your models here.
