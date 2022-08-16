45f3c263e9270b1e25c1dc93f56944463003735e
jeongmu <hot8961@naver.com>
2022-08-16 10:39:46 +0900

    포스트 작성자 추가

--- blog/tests.py
+++ blog/tests.py
@@ -1,9 +1,14 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Post
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_trump = User.objects.create_user(username='trump',
+                                                   password='1q2w3e4r!')
+        self.user_obama = User.objects.create_user(username='obama',
+                                                   password='1q2w3e4r!')
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -37,11 +42,13 @@ class TestView(TestCase):
 
         post_001 = Post.objects.create(
             title='첫 번쩨 포스트입니다,',
-            content='Hello Word. We are the world.'
+            content='Hello Word. We are the world.',
+            author=self.user_trump
         )
         post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
-            content='2등이 전부는 아니잖아요?'
+            content='2등이 전부는 아니잖아요?',
+            author=self.user_obama
         )
         self.assertEqual(Post.objects.count(), 2)
 
@@ -54,6 +61,10 @@ class TestView(TestCase):
         self.assertIn(post_002.title, main_area.text)
 
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+        self.assertIn(post_001.author.username.upper(), main_area.text)
+        self.assertIn(post_002.author.username.upper(), main_area.text)
+
 
     def test_post_detail(self):
         post_001 = Post.objects.create(
