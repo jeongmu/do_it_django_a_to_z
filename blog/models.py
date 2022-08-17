a5df00a04ec624b5c8475b2f95c394a6962ccb0b
jeongmu <hot8961@naver.com>
2022-08-17 09:52:30 +0900

    카테고리 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,14 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+
     class Meta:
         verbose_name_plural = 'Categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     hook_text = models.CharField(max_length=100, blank=True)
