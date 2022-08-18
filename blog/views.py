14725a3056ea101b906c08365a23e04579828ca5
jeongmu <hot8961@naver.com>
2022-08-18 14:07:43 +0900

    마크타운 적용하기

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,9 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Category, Tag
-from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
-
-
 
 class PostList(ListView):
     model = Post
@@ -18,23 +16,24 @@ class PostList(ListView):
         return context
 
 def category_page(request, slug):
-        if slug == 'no_category':
-            category = '미분류'
-            post_list = Post.objects.filter(category=None)
-        else:
-            category = Category.objects.get(slug=slug)
-            post_list = Post.objects.filter(category=category)
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
 
-        return render(
-            request,
-            'blog/post_list.html',
-            {
-                'post_list': post_list,
-                'categories': Category.objects.all(),
-                'no_category_post_count': Post.objects.filter(category=None).count(),
-                'category': category,
-            }
-        )
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
+
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
     post_list = tag.post_set.all()
@@ -56,11 +55,14 @@ class PostDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
-        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        context['no_category_post_count']=Post.objects.filter(category=None).count()
         return context
-class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):
+# Create your views here.
+
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
 
@@ -74,17 +76,19 @@ class PostCreate(LoginRequiredMixin,UserPassesTestMixin,CreateView):
             if tags_str:
                 tags_str = tags_str.strip()
 
-                tags_str = tags_str.replace(',', ';')
+                tags_str = tags_str.replace(',',';')
                 tags_list = tags_str.split(';')
 
-            for t in tags_list:
-                t = t.strip()
-                tag, is_tag_created = Tag.objects.get_or_create(name=t)
-                if is_tag_created:
-                    tag.slug = slugify(t, allow_unicode=True)
-                    tag.save()
-                self.object.tags.add(tag)
+                for t in tags_list:
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+
             return response
+
         else:
             return redirect('/blog/')
 
@@ -93,6 +97,7 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
     template_name = 'blog/post_update_form.html'
+
     def get_context_data(self, **kwargs):
         context = super(PostUpdate, self).get_context_data()
         if self.object.tags.exists():
@@ -100,12 +105,9 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
             for t in self.object.tags.all():
                 tags_str_list.append(t.name)
             context['tags_str_default'] = '; '.join(tags_str_list)
+
         return context
-    def dispatch(self, request, *args, **kwargs):
-        if request.user.is_authenticated and request.user == self.get_object().author:
-            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
-        else:
-            raise PermissionDenied
+
     def form_valid(self, form):
         response = super(PostUpdate, self).form_valid(form)
         self.object.tags.clear()
@@ -123,4 +125,11 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
                     tag.slug = slugify(t, allow_unicode=True)
                     tag.save()
                 self.object.tags.add(tag)
+
         return response
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
